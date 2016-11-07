cab64d550e2e27bc27adfeb2073cdfa61e322466
Enoch2k2 <enoch2k2@hotmail.com>
2016-11-07 11:51:26 -0600

    First Commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	private Rigidbody rb;
+	private int count;
+
+	public Text countText;
+	public Text winText;
+	public int speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		updateCounter ();
+		winText.text = "";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 addMovement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (addMovement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) {
+		count = count + 1;
+		updateCounter ();
+		other.gameObject.SetActive(false);
+	}
+
+	void updateCounter()
+	{
+		countText.text = "Count: " + count;
+		if (count >= 12) 
+		{
+			winText.text = "YOU WIN!";
+		}
+	}
+}
